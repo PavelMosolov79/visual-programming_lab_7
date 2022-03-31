9b39014e4936665dd3b4ac22c86dbb0e3c2bbc96
PavelMosolov79 <78637178+PavelMosolov79@users.noreply.github.com>
2022-03-31 22:38:47 +0700

    main

---
+++ lab7/Models/Grade.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Media;
+using ReactiveUI;
+
+namespace lab7.Models
+{
+    public class Grade : ReactiveObject
+    {
+        public Grade(string FIO)
+        {
+            Val = "0";
+            Fio = FIO;
+        }
+        ISolidColorBrush color;
+        public ISolidColorBrush Color
+        {
+            get => color;
+            set => this.RaiseAndSetIfChanged(ref color, value);
+        }
+
+        public string Fio
+        {
+            get; set;
+        }
+
+
+        string val;
+        public string Val
+        {
+            get => val;
+            set
+            {
+                try
+                {
+                    float val = float.Parse(value);
+                    if (val < 1 ) Color = Brushes.Red;
+                    else if (val <1.5) Color = Brushes.Yellow;
+                    else Color = Brushes.Green;
+                    if (val < 0 || val > 2)
+                    {
+                        value = "#ERROR";
+                        Color = Brushes.White;
+                    }
+                    value = $"{val:0.##}";
+                } catch(FormatException e)
+                {
+                    value = "#ERROR";
+                }
+                this.RaiseAndSetIfChanged(ref val, value);
+            }
+        }
+    }
+}
---
+++ lab7/Models/Student.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Media;
+using ReactiveUI;
+
+namespace lab7.Models
+{
+    public class Student : ReactiveObject
+    {
+        public bool IsChecked
+        {
+            get; set;
+        }
+        public List<Grade> Grades
+        {
+            get;set;
+        }
+
+        string averageGrade;
+
+        ISolidColorBrush color;
+        public ISolidColorBrush Color
+        {
+            get => color;
+            set => this.RaiseAndSetIfChanged(ref color, value);
+        }
+        public string AverageGrade
+        {
+            get => averageGrade;
+            set => this.RaiseAndSetIfChanged(ref averageGrade, value);
+        }
+        void calculate()
+        {
+            float res = 0;
+            try
+            {
+                foreach (var grade in Grades)
+                {
+                    res += int.Parse(grade.Val);
+                }
+            } catch(FormatException e)
+            {
+                AverageGrade = "#ERROR";
+                Color = Brushes.White;
+                return;
+            }
+            res /= Grades.Count;
+            if (res < 1) Color = Brushes.Red;
+            else if (res < 1.5) Color = Brushes.Yellow;
+            else Color = Brushes.Green;
+            AverageGrade = res.ToString("0.00");
+        }
+        public Student()
+        {
+            FIO = "";
+            Grades = new List<Grade> { new Grade("Пред. 1"), new Grade("Пред. 2"), new Grade("Пред. 3"), new Grade("Пред. 4") };
+            for (int i = 0; i < Grades.Count; i++)
+                Grades[i].Changed.Subscribe((x) => calculate());
+            calculate();
+        }
+        public Student(string fio):this()
+        {
+            FIO = fio;
+        }
+        string FIO;
+        public string Fio
+        {
+            get => FIO;
+            set => FIO = value;
+        }
+    }
+}
